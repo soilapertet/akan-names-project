744af741e9b5298a1ef9242abbfeb995ba495c34
Soila Pertet <nicolepertet@gmail.com>
2021-12-10 17:44:41 +0300

    Check the validity of the entries of the form

--- js/akan-scripts.js
+++ js/akan-scripts.js
@@ -5,8 +5,42 @@ var CC = parseInt(year.toString().slice(0,2));
 var YY = parseInt(year.toString().slice(2));
 var MM = document.getElementById("Months").value;
 var DD = document.getElementById("Days").value;
+
+// Create a function to check validity of the entries of the form
+function checkValidity() {
+  if(document.getElementById("Days").value === "days" && document.getElementById("Months").value === "months" &&
+  document.getElementById("Years").value === "years"); {
+    alert("Please choose a valid date, month and year.");
+  } 
+  elseif (document.getElementById("Days").value === "days"); {
+    alert("Please choose a valid date.");
+  } 
+  elseif (document.getElementById("Months").value === "months"); {
+    alert("Please choose a valid month.");
+  } 
+  elseif (document.getElementById("Years").value === "years"); {
+    alert("Please choose a valid year.");
+  }
+  else {
+    return false;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Create a function to calculate the day of the week
-var dayOfTheWeek = function(CC ,YY ,MM ,DD) {
+function  dayOfTheWeek() {
 return Math.round((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+ DD) % 7);
 }
-alert(dayOfTheWeek(20,00,07,20));
+
