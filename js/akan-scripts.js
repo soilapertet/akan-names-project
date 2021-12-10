ebdc364427651968da241b9e0233b694e1b8f294
Soila Pertet <nicolepertet@gmail.com>
2021-12-10 18:16:47 +0300

    Create arrays for both the male and female Ghanian names

--- js/akan-scripts.js
+++ js/akan-scripts.js
@@ -1,3 +1,5 @@
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var year = document.getElementById("Years").value;
 // Isolate century digits
 var CC = parseInt(year.toString().slice(0,2));
@@ -10,37 +12,22 @@ var DD = document.getElementById("Days").value;
 function checkValidity() {
   if(document.getElementById("Days").value === "days" && document.getElementById("Months").value === "months" &&
   document.getElementById("Years").value === "years"); {
-    alert("Please choose a valid date, month and year.");
+    alert("Please choose a valid date, month and year!");
   } 
   elseif (document.getElementById("Days").value === "days"); {
-    alert("Please choose a valid date.");
+    alert("Please choose a valid date!");
   } 
   elseif (document.getElementById("Months").value === "months"); {
-    alert("Please choose a valid month.");
+    alert("Please choose a valid month!");
   } 
   elseif (document.getElementById("Years").value === "years"); {
-    alert("Please choose a valid year.");
+    alert("Please choose a valid year!");
   }
   else {
     return false;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Create a function to calculate the day of the week
 function  dayOfTheWeek() {
 return Math.round((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+ DD) % 7);
 }
-
