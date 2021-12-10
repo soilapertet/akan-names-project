e880b5f50edf73adb7be52172e1dabf1ab8abc4d
Soila Pertet <nicolepertet@gmail.com>
2021-12-10 15:00:46 +0300

    Create a function to calculate the day of the week

--- js/akan-scripts.js
+++ js/akan-scripts.js
@@ -3,3 +3,10 @@ var year = document.getElementById("Years").value;
 var CC = parseInt(year.toString().slice(0,2));
 // Isolate year digits
 var YY = parseInt(year.toString().slice(2));
+var MM = document.getElementById("Months").value;
+var DD = document.getElementById("Days").value;
+// Create a function to calculate the day of the week
+var dayOfTheWeek = function(CC ,YY ,MM ,DD) {
+return Math.round((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+ DD) % 7);
+}
+alert(dayOfTheWeek(20,00,07,20));
