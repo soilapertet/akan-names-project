66995d0b7403450dcffe61799d9f19d543e6cba8
Soila Pertet <nicolepertet@gmail.com>
2021-12-11 04:54:41 +0300

    Make a few changes to HTML and JavaScript files

--- js/akan-scripts.js
+++ js/akan-scripts.js
@@ -1,13 +1,6 @@
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-var year = document.getElementById("Years").value;
-// Isolate century digits
-var CC = parseInt(year.toString().slice(0,2));
-// Isolate year digits
-var YY = parseInt(year.toString().slice(2));
-var MM = document.getElementById("Months").value;
-var DD = document.getElementById("Days").value;
+var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 // Create a function to check validity of the entries of the form
 function checkValidity() {
@@ -30,7 +23,15 @@ function checkValidity() {
 }
 // Create a function to calculate the day of the week
 function  dayOfTheWeek() {
-return Math.round((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+ DD) % 7);
+  var year = document.getElementById("Years").value;
+// Isolate century digits
+  var CC = parseInt(year.toString().slice(0,2));
+// Isolate year digits
+  var YY = parseInt(year.toString().slice(2));
+  var MM = document.getElementById("Months").value;
+  var DD = document.getElementById("Days").value;
+  // Calculate the value for the day of the week
+  return Math.round((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+ DD) % 7);
 }
 // Create a function to assign Akan names based on gender and day of the week
 function assignName() {
@@ -40,51 +41,55 @@ function assignName() {
   elseif (document.getElementById("female").checked === true); {
     var gender = "female";
   }
+  else; {
+    alert("Please choose a gender!");
+    return false;
+  }
   switch(gender) {
     case "male":
       if (dayOfTheWeek === 0); {
-        alert("Congratulations! Your Akan name is "+maleNames[0]+"!");
+        alert("Congratulations! You were born on a "+days[0]+" and your Akan name is "+maleAkanNames[0]+"!");
       }
       elseif (dayOfTheWeek === 1); {
-        alert("Congratulations! Your Akan name is "+maleNames[1]+"!");
+        alert("Congratulations! You were born on a "+days[1]+" and your Akan name is "+maleAkanNames[1]+"!");
       }
       elseif (dayOfTheWeek === 2); {
-        alert("Congratulations! Your Akan name is "+maleNames[2]+"!");
+        alert("Congratulations! You were born on a "+days[2]+" and your Akan name is "+maleAkanNames[2]+"!");
       }
       elseif (dayOfTheWeek === 3); {
-        alert("Congratulations! Your Akan name is "+maleNames[3]+"!");
+        alert("Congratulations! You were born on a "+days[3]+" and your Akan name is "+maleAkanNames[3]+"!");
       }
       elseif (dayOfTheWeek === 4); {
-        alert("Congratulations! Your Akan name is "+maleNames[4]+"!");
+        alert("Congratulations! You were born on a "+days[4]+" and your Akan name is "+maleAkanNames[4]+"!");
       }
       elseif (dayOfTheWeek === 5); {
-        alert("Congratulations! Your Akan name is "+maleNames[5]+"!");
+        alert("Congratulations! You were born on a "+days[5]+" and your Akan name is "+maleAkanNames[5]+"!");
       }
       else {
-        alert("Congratulations! Your Akan name is "+maleNames[6]+"!");
+        alert("Congratulations! You were born on a "+days[6]+" and your Akan name is "+maleAkanNames[6]+"!");
       }
     break;
     case "female":
       if (dayOfTheWeek === 0); {
-        alert("Congratulations! You were born on a "+days[0]+" and your Akan name is "+femaleNames[0]+"!");
+        alert("Congratulations! You were born on a "+days[0]+" and your Akan name is "+femaleAkanNames[0]+"!");
       }
       elseif (dayOfTheWeek === 1); {
-        alert("Congratulations! You were born on a "+days[1]+" and your Akan name is "+femaleNames[1]+"!");
+        alert("Congratulations! You were born on a "+days[1]+" and your Akan name is "+femaleAkanNames[1]+"!");
       }
       elseif (dayOfTheWeek === 2); {
-        alert("Congratulations! You were born on a "+days[2]+" and your Akan name is "+femaleNames[2]+"!");
+        alert("Congratulations! You were born on a "+days[2]+" and your Akan name is "+femaleAkanNames[2]+"!");
       }
       elseif (dayOfTheWeek === 3); {
-        alert("Congratulations! You were born on a "+days[3]+" and your Akan name is "+femaleNames[3]+"!");
+        alert("Congratulations! You were born on a "+days[3]+" and your Akan name is "+femaleAkanNames[3]+"!");
       }
       elseif (dayOfTheWeek === 4); {
-        alert("Congratulations! You were born on a "+days[4]+" and your Akan name is "+femaleNames[4]+"!");
+        alert("Congratulations! You were born on a "+days[4]+" and your Akan name is "+femaleAkanNames[4]+"!");
       }
       elseif (dayOfTheWeek === 5); {
-        alert("Congratulations! You were born on a "+days[5]+" and your Akan name is "+femaleNames[5]+"!");
+        alert("Congratulations! You were born on a "+days[5]+" and your Akan name is "+femaleAkanNames[5]+"!");
       }
       else {
-        alert("Congratulations! You were born on a "+days[6]+" and your Akan name is "+femaleNames[6]+"!");
+        alert("Congratulations! You were born on a "+days[6]+" and your Akan name is "+femaleAkanNames[6]+"!");
       }
     break;
   }
