4af1b11d988599eec76a8695d06bb981fc9509e9
Soila Pertet <nicolepertet@gmail.com>
2021-12-11 18:58:38 +0300

    Create a new function, 'getAkanName()'

--- js/akan-scripts.js
+++ js/akan-scripts.js
@@ -1,3 +1,9 @@
+function getAkanName() {
+  validity();
+  day = dayOfTheWeek();
+  assignName();
+}
+
 var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
